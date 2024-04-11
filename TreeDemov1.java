16dd39a097fecd7e94520a5a431f201cb9e39e31
Jesus Sanchez <sanchez.js126@gmail.com>
2024-04-11 01:54:14 -0700

    'find' implemented

--- TreeDemov1.java
+++ TreeDemov1.java
@@ -118,7 +118,16 @@ class Node{
 	   */
 	   public boolean find(Node root, int key){
 		 //implement in here
-		  
+		 if (root == null) {
+			return false;
+		}
+		if (root.value == key) {
+			return true;
+		} else if (key<root.value) {
+			return find(root.left,key);
+		} else {
+			return find(root.right,key);
+		}
 	   }
 	   
 	   
